06bd8e84e6320a86ef0da216a4d2638ca07eed88
sumaiyakhondoker <sumaiyakhondoker85@gmail.com>
2025-12-25 19:20:17 +0600

    fixed bug

--- backend/src/index.js
+++ backend/src/index.js
@@ -17,9 +17,11 @@ app.use(express.urlencoded({extended: true, limit: "10mb"}))
 
 app.use(cookieParser())
 app.use(cors({
-    origin: "http://localhost:5173",
-    credentials: true
-}))
+  origin: process.env.NODE_ENV === "production"
+    ? true
+    : "http://localhost:5173",
+  credentials: true
+}));
 
 app.use('/api/auth', authRoutes)
 app.use('/api/messages', messageRoutes)
@@ -27,7 +29,7 @@ app.use('/api/messages', messageRoutes)
 if(process.env.NODE_ENV==="production"){
     app.use(express.static(path.join(__dirname, "../frontend/dist")))
 
-    app.get("/*", (req, res) => {
+   app.use((req, res) => {
   res.sendFile(path.join(__dirname, "../frontend/dist/index.html"));
 });
 }
